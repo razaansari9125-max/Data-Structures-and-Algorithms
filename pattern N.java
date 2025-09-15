70612f6232d86e317771c3fdc904251285998749
Raza  Ansari <razaansari9125@gmail.com>
2025-09-15 22:36:31 +0530

    Add files via upload

---
+++ pattern N.java
@@ -0,0 +1,17 @@
+public class N {
+    public static void main(String[] args) {
+        int k=2;
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=5; j++) {
+                if(j==1 || j==5 || j+i==k) {
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+            k=k+2;
+        }
+    }
+}
