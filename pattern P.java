5cd73e8d43611187387e14e45b43205382ccfe10
Raza  Ansari <razaansari9125@gmail.com>
2025-09-19 21:54:42 +0530

    Add files via upload

---
+++ pattern P.java
@@ -0,0 +1,15 @@
+public class P {
+    public static void main(String[] args) {
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=4; j++) {
+                if(j==1 || i==1 || i==3 || (j==4 & i<=2)) {
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println( );
+        }
+    }
+}
