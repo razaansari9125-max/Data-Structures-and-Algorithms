3ba45933405494687137bd4dbe4b025147b46f83
Raza  Ansari <razaansari9125@gmail.com>
2025-09-20 11:26:18 +0530

    Add files via upload

---
+++ Wrapper class Unboxing.java
@@ -0,0 +1,15 @@
+package com.wrapperclass;
+
+public class Unboxing {
+	public static void main(String[] args) {
+		
+		Integer a= 10;
+		int b = a;
+		System.out.println(b);
+		
+		Character ch1 = 'b';
+		char ch2 = ch1;
+		System.out.println(ch2);
+	}
+
+}
