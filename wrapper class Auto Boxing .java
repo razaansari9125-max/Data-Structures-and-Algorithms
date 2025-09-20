f0f4f2879e6f318801e6ff0c091161b7c6859265
Raza  Ansari <razaansari9125@gmail.com>
2025-09-20 11:26:54 +0530

    Add files via upload

---
+++ wrapper class Auto Boxing .java
@@ -0,0 +1,14 @@
+package com.wrapperclass;
+
+public class AutoBoxing {
+	public static void main(String[] args) {
+		int a = 10;
+		Integer b = 20;
+		System.out.println(b);
+		
+		char ch1 = 'a';
+		Character ch2 = ch1;
+		System.out.println(ch2);
+	}
+
+}
