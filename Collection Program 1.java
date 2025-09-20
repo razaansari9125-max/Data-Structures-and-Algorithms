0fd42978f77895a08a32727ac7a9ed2f583ed6b8
Raza  Ansari <razaansari9125@gmail.com>
2025-09-20 11:28:07 +0530

    Add files via upload

---
+++ Collection Program 1.java
@@ -0,0 +1,18 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class Program1 {
+	public static void main(String[] args) {
+		ArrayList a = new ArrayList();
+		a.add(10);
+		a.add(20);
+		a.add(30);
+		a.add(40);
+		a.add(50);
+		a.add(60);
+		System.out.println(a);
+		
+	}
+
+}
