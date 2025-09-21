90d7a994863839086471857749a53b25c0a2664c
Raza  Ansari <razaansari9125@gmail.com>
2025-09-21 13:03:23 +0530

    Add files via upload

---
+++ Array reverse array.java
@@ -0,0 +1,12 @@
+package com.Array;
+ //Question1= Write a program to print array element in reverse order.
+public class ReverseArray {
+	public static void main(String[] args) {
+		
+		int[] array = {10,20,30,40,50,60};
+		for(int i=array.length-1; i>=0; i--)
+		{
+			System.out.println(array[i]);
+		}
+	}
+}
