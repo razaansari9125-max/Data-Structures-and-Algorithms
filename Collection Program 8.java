814456c3b913a76752d86a859b42d1900ea29368
Raza  Ansari <razaansari9125@gmail.com>
2025-09-25 11:54:02 +0530

    Add files via upload

---
+++ Collection Program 8.java
@@ -0,0 +1,19 @@
+package com.collection;
+
+import java.util.ArrayList;
+
+public class Programs8 {
+	public static void main(String[] args) {
+		
+		ArrayList<Integer> a = new ArrayList<>();
+		a.add(36);
+		a.add(67);
+		a.add(89);
+		a.add(66786);
+		
+		for(int i: a)
+		{
+			System.out.println(i);
+		}
+	}
+}
