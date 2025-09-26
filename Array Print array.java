ef11ee83bf7c1e2651af0dad3e72ea5748669e66
Raza  Ansari <razaansari9125@gmail.com>
2025-09-26 18:13:26 +0530

    Add files via upload

---
+++ Array Print array.java
@@ -0,0 +1,12 @@
+package com.Array;
+
+public class PrintArray {
+	public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+
+        System.out.println("Array elements numbers:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
