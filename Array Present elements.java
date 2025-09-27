5aad8b99ad4e6c763fd20a23a94e69b10ee0bd08
Raza  Ansari <razaansari9125@gmail.com>
2025-09-27 21:34:53 +0530

    Add files via upload

---
+++ Array Present elements.java
@@ -0,0 +1,10 @@
+package com.Array;
+
+public class PresentElements {
+	public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50, 60};
+
+        System.out.println("Number of elements in the array: " + arr.length);
+    }
+
+}
