87908ef98f2c1ae01dbcff3a6586bbc05958bad2
Raza  Ansari <razaansari9125@gmail.com>
2025-09-28 18:40:47 +0530

    Add files via upload

---
+++ Array Descending.java
@@ -0,0 +1,21 @@
+package com.Array;
+
+public class DescendingSort {
+	public static void main(String[] args) {
+        int[] arr = {50, 30, 40, 20, 10, 60};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] < arr[j]) {
+                    
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
