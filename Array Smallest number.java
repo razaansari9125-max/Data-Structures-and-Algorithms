081cf1c65340ec21da40741dc7227e4c13df8f98
Raza  Ansari <razaansari9125@gmail.com>
2025-09-29 21:06:25 +0530

    Add files via upload

---
+++ Array Smallest number.java
@@ -0,0 +1,21 @@
+package com.Array;
+
+public class SmallestNumber {
+	public static void main(String[] args) {
+
+        int[] arr = {9, 2, 4, 1, 7, 6, 3, 8, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        System.out.println("Samllest number is: " + arr[0]);
+    }
+
+
+}
