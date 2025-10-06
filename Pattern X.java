a841c6e7ccb48dcfc6d115c700e1f1958e7c3cd7
Raza  Ansari <razaansari9125@gmail.com>
2025-10-06 20:01:47 +0530

    Add files via upload

---
+++ Pattern X.java
@@ -0,0 +1,17 @@
+public class X {
+    public static void main(String[] args) {
+        
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=5; j++) {
+                if(j==i || j+i==6) {
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println( );
+            
+        }
+    }
+}
