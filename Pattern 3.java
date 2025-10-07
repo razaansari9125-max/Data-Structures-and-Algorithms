0be451ca17cffc7af367d8159adf468d3d9ad1e3
Raza  Ansari <razaansari9125@gmail.com>
2025-10-07 17:37:39 +0530

    Add files via upload

---
+++ Pattern 3.java
@@ -0,0 +1,20 @@
+package com.Pattern;
+
+public class Pattern3 {
+public static void main(String[] args) {
+        int n=5;
+        for(int i=1; i<=5; i++) {
+            for(int j=1; j<=5; j++) {
+                if(i+j>=n+1) {
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println( );
+            
+        }
+    }
+
+}
