6ca818c7bf75496bf38258dcf54c994702c427d1
Raza  Ansari <razaansari9125@gmail.com>
2025-10-08 19:46:52 +0530

    Add files via upload

---
+++ Pattern 5.java
@@ -0,0 +1,21 @@
+package com.Pattern;
+
+public class Pattern5 {
+public static void main(String[] args) {
+        
+	int n = 9;
+        for(int i=1; i<=9; i++) {
+            for(int j=1; j<=9; j++) {
+                if(i>=j && i+j<=n-1)  {
+                    System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println( );
+            
+        }
+    }
+
+}
