2b9bc33315391f992a0b3940269a5867d9453447
Raza  Ansari <razaansari9125@gmail.com>
2025-10-09 18:19:56 +0530

    Add files via upload

---
+++ Number Pattern 4.java
@@ -0,0 +1,23 @@
+package com.NumbersPattern;
+
+public class Pattern4 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0; i<n; i++)
+		{
+			int k=1;
+			for(int j=0; j<n; j++)
+			{
+				if(i>=j)
+				{
+					System.out.print(k++%2 +" ");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+}
