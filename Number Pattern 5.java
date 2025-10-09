b88f384d20a84d25d319cea6f6acba8c61ab1342
Raza  Ansari <razaansari9125@gmail.com>
2025-10-09 18:20:35 +0530

    Add files via upload

---
+++ Number Pattern 5.java
@@ -0,0 +1,24 @@
+package com.NumbersPattern;
+
+public class Pattern5 {
+	public static void main(String[] args) {
+		int n=5;
+		char ch = 'A';
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(i>=j)
+				{
+					System.out.print(ch++ +" ");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+}
