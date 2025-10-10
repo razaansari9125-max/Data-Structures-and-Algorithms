08117ee017d147d08bc06c7881553b603258bb0c
Raza  Ansari <razaansari9125@gmail.com>
2025-10-10 20:16:55 +0530

    Add files via upload

---
+++ Number Pattern 8.java
@@ -0,0 +1,25 @@
+package com.NumbersPattern;
+
+public class Pattern8 {
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0; i<n; i++)
+		{
+			int k=1;
+			for(int j=0; j<n; j++)
+			{
+				if(i>=j)
+				{
+					System.out.print(k++ +" ");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+
+}
