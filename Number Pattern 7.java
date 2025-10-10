e4088ef4c0f408d4a56a3eb09d9918c7d5cc5a30
Raza  Ansari <razaansari9125@gmail.com>
2025-10-10 20:16:12 +0530

    Add files via upload

---
+++ Number Pattern 7.java
@@ -0,0 +1,25 @@
+package com.NumbersPattern;
+
+public class Pattern7 {
+	public static void main(String[] args) {
+		int n=5;
+		int k=1;
+		for(int i=0; i<n; i++)
+		{
+			for(int j=0; j<n; j++)
+			{
+				if(i>=j)
+				{
+					System.out.print(k++%2 +" ");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
+
+
+}
