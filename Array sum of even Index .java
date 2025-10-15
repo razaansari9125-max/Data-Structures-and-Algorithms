b77d7a01a78feaa0edc6746464fa939829c4757a
Raza  Ansari <razaansari9125@gmail.com>
2025-10-15 22:28:33 +0530

    Add files via upload

---
+++ Array sum of even Index .java
@@ -0,0 +1,18 @@
+package com.Array;
+
+public class SumOfEvenIndex {
+	public static void main(String[] args) {
+		
+		int []arr = {11, 12, 9, 8, 4};
+		int sum = 0;
+		for(int i=0; i<arr.length; i++)
+		{
+			if(i%2==0)
+			{
+				sum = sum + arr[i];
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
