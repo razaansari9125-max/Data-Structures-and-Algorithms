f0288efc6dc93bede3f6f2e700906042c8b88f7a
Raza  Ansari <razaansari9125@gmail.com>
2025-10-15 22:27:49 +0530

    Add files via upload

---
+++ Array sum of all odd.java
@@ -0,0 +1,17 @@
+package com.Array;
+
+public class SumOfAllOdd {
+	public static void main(String[] args) {
+		int []arr = {3, 4, 5, 6, 7, 8, 9};
+		int sum = 0;
+		for(int i=0; i<arr.length; i++)
+		{
+			if(arr[i]%2==1)
+			{
+				sum = sum + arr[i];
+			}
+		}
+		System.out.println(sum);
+	}
+
+}
