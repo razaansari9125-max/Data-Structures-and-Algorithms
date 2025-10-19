4753195b321188a22cfa538a1e1dcc6cb6e24949
Raza  Ansari <razaansari9125@gmail.com>
2025-10-19 21:46:39 +0530

    Add files via upload

---
+++ Introduction Variables example.java
@@ -0,0 +1,15 @@
+class VariablesExample {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 25000.50;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "Raza";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+    }
+}
