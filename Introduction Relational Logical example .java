3db73341cd7f3ec11e7055567a86be1657cbbdf7
Raza  Ansari <razaansari9125@gmail.com>
2025-10-20 10:42:51 +0530

    Add files via upload

---
+++ Introduction Relational Logical example .java
@@ -0,0 +1,15 @@
+class RelationalLogicalExample {
+    public static void main(String[] args) {
+        int x = 10, y = 20;
+
+        System.out.println("x == y : " + (x == y));
+        System.out.println("x != y : " + (x != y));
+        System.out.println("x > y  : " + (x > y));
+        System.out.println("x < y  : " + (x < y));
+
+        boolean a = true, b = false;
+        System.out.println("a && b : " + (a && b));
+        System.out.println("a || b : " + (a || b));
+        System.out.println("!a     : " + (!a));
+    }
+}
