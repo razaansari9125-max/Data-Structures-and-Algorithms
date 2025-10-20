e37d91e91708a6809c9896ac2cea91360d02bb97
Raza  Ansari <razaansari9125@gmail.com>
2025-10-20 10:41:44 +0530

    Add files via upload

---
+++ Introduction Data Types.java
@@ -0,0 +1,17 @@
+class DataTypes {
+    public static void main(String[] args) {
+        int num = 25;
+        float f = 10.5f;
+        double d = 20.12345;
+        char c = 'A';
+        boolean b = true;
+        String s = "Java";
+
+        System.out.println("Integer: " + num);
+        System.out.println("Float: " + f);
+        System.out.println("Double: " + d);
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + b);
+        System.out.println("String: " + s);
+    }
+}
