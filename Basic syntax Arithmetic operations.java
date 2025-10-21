411a09431d2e9de3c6cc4bf5603d27dece27aa1e
Raza  Ansari <razaansari9125@gmail.com>
2025-10-21 12:07:33 +0530

    Add files via upload

---
+++ Basic syntax Arithmetic operations.java
@@ -0,0 +1,10 @@
+class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 15, b = 4;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Remainder: " + (a % b));
+    }
+}
