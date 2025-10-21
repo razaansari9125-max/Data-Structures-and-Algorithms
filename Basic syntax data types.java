f1788d0631cb7f45b37741fbffdb54f9b5958e56
Raza  Ansari <razaansari9125@gmail.com>
2025-10-21 12:09:07 +0530

    Add files via upload

---
+++ Basic syntax data types.java
@@ -0,0 +1,23 @@
+class DataTypesExample {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 1000;
+        int i = 10000;
+        long l = 100000L;
+        float f = 10.5f;
+        double d = 123.456;
+        char c = 'J';
+        boolean bool = true;
+        String str = "Java";
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+        System.out.println("String: " + str);
+    }
+}
