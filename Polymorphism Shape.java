ae045d5c458e27203ad129dfd1671a4caf1a655f
Raza  Ansari <razaansari9125@gmail.com>
2025-10-27 11:41:33 +0530

    Add files via upload

---
+++ Polymorphism Shape.java
@@ -0,0 +1,27 @@
+abstract class Shape {
+    abstract void draw();
+}
+
+class Circle extends Shape {
+    void draw() {
+        System.out.println("Drawing Circle");
+    }
+}
+
+class Rectangle extends Shape {
+    void draw() {
+        System.out.println("Drawing Rectangle");
+    }
+}
+
+public class AbstractExample {
+    public static void main(String[] args) {
+        Shape s;
+
+        s = new Circle();
+        s.draw();
+
+        s = new Rectangle();
+        s.draw();
+    }
+}
