442bef3bede3ab59ebe4c9def11300275acc7025
Raza  Ansari <razaansari9125@gmail.com>
2025-11-01 19:56:02 +0530

    Add files via upload

---
+++ Package StaticImport Example.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.*; 
+
+public class StaticImportExample {
+    public static void main(String[] args) {
+        System.out.println("Square root of 16 is: " + sqrt(16));
+        System.out.println("Power (2^3) is: " + pow(2, 3));
+    }
+}
