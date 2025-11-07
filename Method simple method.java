4029024735d2ab683eb6c274450836214564b150
Raza  Ansari <razaansari9125@gmail.com>
2025-11-07 22:29:35 +0530

    Add files via upload

---
+++ Method simple method.java
@@ -0,0 +1,10 @@
+class SimpleMethod {
+    void display() {
+        System.out.println("Hello, this is a simple method!");
+    }
+
+    public static void main(String[] args) {
+        SimpleMethod obj = new SimpleMethod();
+        obj.display();
+    }
+}
