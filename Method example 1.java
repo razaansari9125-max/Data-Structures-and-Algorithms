44ba07a17e7a496f024b978645b4dc2f0b8cc70f
Raza  Ansari <razaansari9125@gmail.com>
2025-11-08 22:41:38 +0530

    Add files via upload

---
+++ Method example 1.java
@@ -0,0 +1,11 @@
+class Example1 {
+    // Method definition
+    void displayMessage() {
+        System.out.println("Hello! This is a simple Java method.");
+    }
+
+    public static void main(String[] args) {
+        Example1 obj = new Example1();  // Create object
+        obj.displayMessage();           // Call the method
+    }
+}
